ee65aea6b8e76535272ad21aa59ea1e0ff9d5db5
Jordan Heinrichs <mail@jordanheinrichs.com>
2017-05-25 23:32:17 -0600

    Add RabbitMQ consumer

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 /**
  * Module dependencies.
  */
+const amqp = require('amqplib');
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
@@ -12,6 +13,11 @@ const expressStatusMonitor = require('express-status-monitor');
 const cors = require('cors');
 
 /**
+ * Constants
+ */
+const amqpQueueName = 'telem';
+
+/**
  * Controllers (route handlers).
  */
 // TODO
@@ -21,20 +27,45 @@ const cors = require('cors');
  */
 const app = express();
 
-
 /**
  * Express configuration.
  */
 app.set('port', process.env.PORT || 4000);
-app.use(cors({
-  exposedHeaders: ['Link'],
-}));
+app.use(
+  cors({
+    exposedHeaders: ['Link'],
+  })
+);
 app.use(expressStatusMonitor());
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
+
+// Consumer
+amqp
+  .connect('amqp://localhost')
+  .then(function(conn) {
+    process.once('SIGINT', function() {
+      conn.close();
+    });
+    return conn.createChannel().then((ch) => {
+        ch.assertQueue(amqpQueueName, {exclusive: true})
+        .then(() => {
+          return ch.consume(
+            amqpQueueName,
+            (message) => {
+               // TODO: Set content directly into database
+              console.log(' [x] Received \'%s\'', message.content.toString());
+            },
+            {noAck: false}
+          );
+        })
+        .then(() => console.log(' [*] Waiting for logs. To exit press CTRL+C'));
+    });
+  })
+  .catch(console.warn);
 
 /**
  * Primary app routes.
@@ -44,7 +75,7 @@ app.use(expressValidator());
 /**
  * Static serving for images
  */
-app.use( '/img', express.static(__dirname + '/assets'));
+app.use('/img', express.static(__dirname + '/assets'));
 
 /**
  * Error Handler.
@@ -55,7 +86,12 @@ app.use(errorHandler());
  * Start Express server.
  */
 app.listen(app.get('port'), () => {
-  console.log('%s Express server listening on port %d in %s mode.', chalk.green('✓'), app.get('port'), app.get('env'));
+  console.log(
+    '%s Express server listening on port %d in %s mode.',
+    chalk.green('✓'),
+    app.get('port'),
+    app.get('env')
+  );
 });
 
 module.exports = app;
