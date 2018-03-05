933183350e1b1bda7ec3c2c379411b59ff1d5d65
Nguyen-Anh Tran <nguyen.tran@outlook.com>
2018-03-04 21:54:41 -0700

    fixed travis errors

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+// var favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup for dev purposes
 app.set('views', path.join(__dirname, 'views'));
@@ -18,16 +17,15 @@ app.set('view engine', 'pug');
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ server/test/app.js
@@ -0,0 +1,11 @@
+
+const request = require('supertest');
+const app = require('../app.js');
+
+describe('GET /random-url', () => {
+  it('should return 404', (done) => {
+    request(app)
+      .get('/reset')
+      .expect(404, done);
+  });
+});
