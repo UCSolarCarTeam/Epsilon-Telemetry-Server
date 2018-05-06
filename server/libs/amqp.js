6016897df301c5e671a1299492b2961e3f3ee336
Nguyen-Anh Tran <nguyen.tran@outlook.com>
2018-05-06 02:56:34 -0600

    switched over to postgres promise wrapper. increased logging verbose level and added database handlers.

--- server/libs/amqp.js
+++ server/libs/amqp.js
@@ -1,47 +1,36 @@
 const amqp = require('amqplib');
 const BSON = require('bson');
 const promise = require('promise');
-const db = require('./database');
-const wss = require('./websocket').websocket
 const config = require('../config.json');
-const bson = new BSON();
+const db = require('./database').db;
+const mapJsonToColumns = require('./database').mapColumns;
+const wss = require('./websocket').websocket
+const bson = require('./websocket').bson;
 
 /**
  * Get column names and store in global variable.
  */
 console.log('Updating column names...');
 let columnNames = '';
-db.pool.connect(function(err, client, done) {
-  if (err) {
-    return console.error(db.errors.CONNECT_ERROR, err.stack);
-  }
-  const query = 'SELECT column_name FROM information_schema.columns ' +
-                'WHERE table_schema = \'public\' AND table_name = \'packet\'';
-  client.query(query, function(err, result) {
-    done();
-    if (err) {
-      return console.error(db.errors.SELECT_ERROR, err.stack);
-    }
-
-    // build column name list and remove trailing comma and whitespace
-    for (let i = 1; i < result.rows.length; i++) {
-      columnNames += result.rows[i].column_name + ', ';
-    }
-    columnNames = columnNames.replace(/,\s*$/, '');
-  });
+db.any({
+  name: 'update-columns',
+  text: 'SELECT column_name FROM information_schema.columns ' +
+        'WHERE table_schema = \'public\' AND table_name = \'packet\''
+}).then(columns => {
+  columnNames = columns.slice(1).map(column => column.column_name).join();
+  console.log('Column names updated');
 });
-console.log('Column names updated');
 
 /**
  * Setup the AMQP channel with RabbitMQ (change host if needed)
  */
 amqp.connect(config.rabbitmq.host)
   // connect with amqp server
-  .then(function(conn) {
+  .then(conn => {
     return conn.createChannel();
   })
   // check if exchange exists and is valid
-  .then(function(ch) {
+  .then(ch => {
     const ex = config.rabbitmq.exchange_name;
     const q = config.rabbitmq.queue_name;
     ch.assertExchange(ex, 'fanout', {durable: false});
@@ -49,43 +38,25 @@ amqp.connect(config.rabbitmq.host)
 
     // bind internal queue with external exchange and enter ready state
     return ch.bindQueue(q.queue, ex, '')
-      .then(function(ok) {
+      .then(ok => {
         console.log('Express: waiting for messages in %s', q);
 
         // start reading objects from the queue
         return ch.consume(q, function(msg) {
           const jsonObj = JSON.parse(msg.content);
 
-          // connect to database and check for errors
-          db.pool.connect(function(err, client, done) {
-            if (err) {
-              return console.error(db.errors.CONNECT_ERROR, err.stack);
-            }
+          // insert into database
+          db.one({
+            name: 'rabbitmq-insert',
+            text: `INSERT INTO packet(${columnNames}) values(${mapJsonToColumns(jsonObj)}) RETURNING row_to_json(packet)`
+          }).then(inserted_row => {
+            console.log('1 row inserted from RabbitMQ');
 
-            // insert into database and return inserted row as JSON
-            const query = 'INSERT INTO packet(' + columnNames + ') ' +
-                          'values(' + db.mapJsonToColumns(jsonObj) + ') ' +
-                          'RETURNING row_to_json(packet)';
-            client.query(query, function(err, result) {
-              done();
-              if (err) {
-                return console.error(db.errors.INSERT_ERROR, err.stack);
-              }
-              console.log('1 row inserted from RabbitMQ');
-
-              // serialize the data in the proper format
-              let serData = [];
-              serData.push(result.rows[0].row_to_json)
-              serData = bson.serialize(serData);
-
-              // broadcast inserted row to all connected clients
-              wss.broadcast = function broadcast(serData) {
-                wss.clients.map(client => {
-                  // returns true if send success, false if socket is no longer open
-                  return client.send(serData);
-                });
-              };
-            });
+            // serialize the data and broadcast it to all connected clients
+            serializedData = bson.serialize(inserted_row.row_to_json);
+            wss.broadcast = function broadcast(serializedData) {
+              wss.clients.map(client => client.send(serializedData));
+            };
           });
         }, {noAck: true});
       });
--- server/libs/websocket.js
+++ server/libs/websocket.js
@@ -1,72 +1,50 @@
 const WebSocket = require('ws');
-const db = require('./database');
 const BSON = require('bson');
+const db = require('./database').db;
 const server = require('../bin/www').server;
 
 const bson = new BSON();
 const wss = new WebSocket.Server({server});
 
 const errors = {
-  CONNECT_ERROR: bson.serialize({Error: 'Node couldn\'t connect to PostgreSQL. See Node console for details.'}),
   SELECT_ERROR: bson.serialize({Error: 'Node returned an error during the SELECT query. See Node console for details.'}),
 };
 
 wss.on('connection', function(ws, req) {
-  pool.connect(function(err, client, done) {
-    if (err) {
-      wss.send(errors.CONNECT_ERROR);
-      return console.error(db.errors.CONNECT_ERROR, err.stack);
-    }
-
-    // when client connects, select row with latest telemetry information
-    // as a JSON object and send it
-    const query = 'SELECT json_agg(latest) ' +
-                  'FROM (SELECT * ' +
-                        'FROM packet ' +
-                        'ORDER BY timestamp DESC LIMIT 1) latest';
-    client.query(query, function(err, result) {
-      done();
-        if (err) {
-          ws.send(errors.SELECT_ERROR);
-          return console.error(db.errors.SELECT_ERROR, err.stack);
-        }
-
-        // send latest telemetry row to client (array with 1 element)
-        wss.send(bson.serialize(result.rows));
-    });
+  // when client connects, select row with latest telemetry
+  // information as a JSON object and send it
+  db.one({
+    name: 'client-init',
+    text: 'SELECT json_agg(latest) ' +
+          'FROM (SELECT * ' +
+                'FROM packet ' +
+                'ORDER BY timestamp DESC LIMIT 1) latest'
+  }).then(result => {
+    ws.send(bson.serialize(result.json_agg));
+  }).catch(error => {
+    ws.send(errors.SELECT_ERROR);
   });
 
   // get_past_data event handler to view historical telemetry data
   // MUST receive a JSON object with fields 'start' and 'end' with timestamps
   ws.on('get_past_data', function(msg) {
-    pool.connect(function(err, client, done) {
-      if (err) {
-        ws.send(errors.CONNECT_ERROR);
-        return console.error(db.errors.CONNECT_ERROR, err.stack);
-      }
-
-      // select rows that fall within the time window specified and return as JSON
-      const jsonObj = bson.deserialize(msg);
-      const query = 'SELECT json_agg(rows) ' +
-                    'FROM (SELECT * ' +
-                          'FROM packet ' +
-                          'WHERE timestamp > \'' + str(jsonObj.start) + '\' ' +
-                          'AND timestamp < \'' + str(jsonObj.end) + '\' ' +
-                          'ORDER BY timestamp) rows';
-      client.query(query, function(err, result) {
-        done();
-        if (err) {
-          ws.send(errors.SELECT_ERROR);
-          return console.error(db.errors.SELECT_ERROR, err.stack);
-        }
-
-        // send the rows back in an array
-        ws.send(bson.serialize(result.rows));
-      });
+    db.any({
+      name: 'client-historical-data',
+      text: 'SELECT json_agg(rows) ' +
+            'FROM (SELECT * ' +
+                  'FROM packet ' +
+                  `WHERE timestamp > '${str(jsonObj.start)}' ` +
+                  `AND timestamp < '${str(jsonObj.end)}' ` +
+                  'ORDER BY timestamp) rows'
+    }).then(results => {
+      ws.send(bson.serialize(result.rows));
+    }).catch(error => {
+      ws.send(errors.SELECT_ERROR);
     });
   });
 
   // add more event handlers here if needed
 });
 
+module.exports.bson = bson;
 module.exports.websocket = wss;
