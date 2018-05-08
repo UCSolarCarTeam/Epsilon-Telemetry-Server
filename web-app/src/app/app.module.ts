ab75c8130c3040d850200a81b41949fe6a60e48f
henrytran28 <henrytrann28@gmail.com>
2018-05-07 22:52:26 -0600

    made tabs more pretty and looking closer to design

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BatteryComponent } from './tabs/battery/battery.component';
@@ -23,6 +24,7 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     MatTabsModule,
