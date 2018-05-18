4777ebb0ab5cf1bc01b0f052265c194134a40475
henrytran28 <henrytrann28@gmail.com>
2018-05-18 17:33:12 -0600

    fix linting

--- web-app/src/app/battery-misc.ts
+++ web-app/src/app/battery-misc.ts
@@ -1,9 +1,9 @@
 export enum State {
-  OFF = "Off",
-  COMMON_ENGAGED = "Common Engaged",
-  CHARGE_ENGAGED = "Charge Engaged",
-  DISCHARGE_ENGAGED = "Discharge Engaged",
-  ALL_ENGAGED = "All engaged"
+  OFF = 'Off',
+  COMMON_ENGAGED = 'Common Engaged',
+  CHARGE_ENGAGED = 'Charge Engaged',
+  DISCHARGE_ENGAGED = 'Discharge Engaged',
+  ALL_ENGAGED = 'All engaged'
 }
 
 export class BatteryMisc {
