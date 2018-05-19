ef1451f740540dd35293a873edb606b1d38b222f
Nathanael Huh <nathanael.huh@ucalgary.ca>
2018-05-19 15:48:15 -0600

    added controls

--- web-app/src/app/tabs/controls/controls.component.ts
+++ web-app/src/app/tabs/controls/controls.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controls.component.css']
 })
 export class ControlsComponent implements OnInit {
+	headlightsOff = false;
+	headlightsLow = true;
+	headlightsHigh = false;
+	signalLeft = true;
+	signalRight = false;
+	hazard = true;
+	interior = true;
+	pushToTalk = true;
+	horn = true;
+	aux = true;
+	songPrev = true;
+	songNext = false;
+	volumeUp = true;
+	volumeDown = false;
+	brakes = true;
+	forward = true;
+	reverse = false;
+	reset = true;
 
   constructor() { }
 
