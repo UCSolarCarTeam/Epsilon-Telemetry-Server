56f6d8fa3b36fd5a462ecfe3485d3d74cb7d5f20
henrytran28 <henrytrann28@gmail.com>
2018-05-31 20:57:48 -0600

    added min-width and finished all controls/lights

--- web-app/src/app/rightpanel/rightpanel.component.ts
+++ web-app/src/app/rightpanel/rightpanel.component.ts
@@ -7,6 +7,10 @@ import { Battery } from '../_objects/battery';
 import { BatteryService } from '../_services/battery.service';
 import { AuxBms } from '../_objects/aux-bms';
 import { AuxBmsService } from '../_services/aux-bms.service';
+import { Controls } from '../_objects/controls';
+import { ControlsService } from '../_services/controls.service';
+import { Lights } from '../_objects/lights';
+import { LightsService } from '../_services/lights.service';
 
 @Component({
   selector: 'app-rightpanel',
@@ -17,6 +21,8 @@ export class RightpanelComponent implements OnInit {
 
   battery: Battery;
   auxBms: AuxBms;
+  lights: Lights;
+  controls: Controls;
 
   carMisc = new CarMisc(false, true, false, true,
                         30, 40, 50, 20, 10, 140,
@@ -29,16 +35,19 @@ export class RightpanelComponent implements OnInit {
     highestCellTemp: 15,
     lowestCellVoltage: 18,
     packCurrent: 10,
-    packVoltage: 50,
-    prechargeState: State.COMMON_ENGAGED
+    packVoltage: 50, prechargeState: State.COMMON_ENGAGED
   };
 
   constructor(private batteryService: BatteryService,
-              private auxBmsService: AuxBmsService) { }
+              private auxBmsService: AuxBmsService,
+              private controlsService: ControlsService,
+              private lightsService: LightsService) { }
 
   ngOnInit() {
     this.battery = this.batteryService.getData();
     this.auxBms = this.auxBmsService.getData();
+    this.lights = this.lightsService.getData();
+    this.controls = this.controlsService.getData();
 
     this.batteryService.battery$.subscribe(
       (data: Battery) => {
@@ -49,6 +58,18 @@ export class RightpanelComponent implements OnInit {
     this.auxBmsService.auxbms$.subscribe(
       (data: AuxBms) => {
         this.auxBms = data;
+      }
+    );
+
+    this.controlsService.controls$.subscribe(
+      (data: Controls) => {
+        this.controls = data;
+      }
+    );
+
+    this.lightsService.lights$.subscribe(
+      (data: Lights) => {
+        this.lights = data;
       }
     );
   }
