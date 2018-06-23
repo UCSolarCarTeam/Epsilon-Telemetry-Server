ccaf8495e21c3697a9ccde7e8bd0df0af4ec54ed
Nathanael Huh <nathanael.huh@ucalgary.ca>
2018-06-22 19:26:39 -0600

    this should be a mostly functional version

--- web-app/src/app/tabs/motor/motor.component.ts
+++ web-app/src/app/tabs/motor/motor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Motor } from '../../_objects/motor';
+import { MotorService } from '../../_services/motor.service';
+
 @Component({
   selector: 'app-motor',
   templateUrl: './motor.component.html',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotorComponent implements OnInit {
 
-  constructor() { }
+  motor0: Motor;
+  motor1: Motor;
+
+  constructor(private motorService: MotorService) { }
 
   ngOnInit() {
+    this.motor0 = this.motorService.getData(0);
+    this.motor1 = this.motorService.getData(1);
+
+    this.motorService.motor0$.subscribe(
+      (data: Motor) => {
+        this.motor0 = data;
+      }
+    );
+
+    this.motorService.motor1$.subscribe(
+      (data: Motor) => {
+        this.motor1 = data;
+      }
+    );
   }
 
 }
--- web-app/src/app/tabs/mppt/mppt.component.ts
+++ web-app/src/app/tabs/mppt/mppt.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MPPT } from '../../_objects/mppt';
+import { MPPTService } from '../../_services/mppt.service';
+
 @Component({
   selector: 'app-mppt',
   templateUrl: './mppt.component.html',
@@ -22,11 +25,35 @@ export class MpptComponent implements OnInit {
 	mppt2BatteryVoltage = 0;
 	mppt2Temperature = 0;
 
+  mppt0: MPPT;
+  mppt1: MPPT;
+  mppt2: MPPT;
 
 
-  constructor() { }
+  constructor(private mpptService: MPPTService) { }
 
   ngOnInit() {
+    this.mppt0 = this.mpptService.getData(0);
+    this.mppt1 = this.mpptService.getData(1);
+    this.mppt2 = this.mpptService.getData(2);
+
+    this.motorService.motor0$.subscribe(
+      (data: Motor) => {
+        this.motor0 = data;
+      }
+    );
+
+    this.mpptService.mppt1$.subscribe(
+      (data: MPPT) => {
+        this.mppt1 = data;
+      }
+    );
+
+    this.mpptService.mppt2$.subscribe(
+      (data: MPPT) => {
+        this.mppt2 = data;
+      }
+    );
   }
 
 }
