9b343e005a39ce5ef9ba48995cad2b1c7c2aec61
Nguyen-Anh Tran <nguyen.tran@outlook.com>
2018-07-28 14:29:59 -0600

    remove view stuff

--- server/app.js
+++ server/app.js
@@ -11,21 +11,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '../web-app/dist')));
 
-/**
- * Routes
- */
-// Main page
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../web-app/dist/index.html'));
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
