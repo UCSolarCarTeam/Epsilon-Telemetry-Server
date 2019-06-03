e0031456b4e92c114e6fea06ed6f9d393b9b6cf9
Bill Luu <billluu619@gmail.com>
2019-06-02 18:40:07 -0600

    Get all laps from database and send them to client
    When the client opens the page, all the laps in the database will be sent to them.

--- server/scripts/websocket.js
+++ server/scripts/websocket.js
@@ -11,7 +11,7 @@ const errors = {
 wss.on('connection', function(ws, req) {
   // when client connects, fetch last row in db
   console.log('New client connected!');
-  db.last()
+  db.lastPacket()
     // send to client
     .then((lastRow) => {
       // make the datetime pretty
@@ -22,6 +22,14 @@ wss.on('connection', function(ws, req) {
     .catch((err) => {
       ws.send({error: errors.SELECT_ERROR});
     });
+  db.laps()
+  .then((laps) => {
+    // console.log(laps)
+    for (let lap = 0; lap < laps.length; lap++) {
+      laps[lap]['msgType'] = 'lap';
+      ws.send(JSON.stringify(laps[lap]));
+    }
+  });
 });
 
 wss.broadcast = function broadcast(data) {
