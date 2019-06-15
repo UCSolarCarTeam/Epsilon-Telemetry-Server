ae2a3ce77c95da57d796967f4fa42ba60ed2c681
Bill Luu <billluu619@gmail.com>
2019-06-15 13:58:50 -0600

    Fix compiling issues

--- web-app/src/app/tabs/race/data-table/data-table.component.ts
+++ web-app/src/app/tabs/race/data-table/data-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort } from '@angular/material';
 import { DataTableDataSource } from './data-table-datasource';
-import { ILapDataInterface } from '../../../_objects/interfaces/lap-data.interface';
+import { LapData } from '../../../_objects/lapData';
 
 @Component({
   selector: 'app-data-table',
@@ -12,14 +12,14 @@ export class DataTableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   dataSource: DataTableDataSource;
-  lapData: ILapDataInterface[] = {
+  lapData: LapData[] = [{
     'lapNumber': 0,
     'lapTime': 'time',
     'totalPowerIn': 0,
     'totalPowerOut': 0,
     'netPowerOut': 0,
     'distance': 0
-  }
+  }]
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['lapNumber', 'lapTime', 'totalPowerIn', 'totalPowerOut', 'netPowerOut', 'distance'];
