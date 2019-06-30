4bdbc6c0896418ba6e989a914db74d4d5c86e0bc
Bill Luu <billluu619@gmail.com>
2019-06-29 19:44:30 -0600

    Implement rounding service to race page

--- web-app/src/app/_services/lap.service.ts
+++ web-app/src/app/_services/lap.service.ts
@@ -2,6 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { ILapDataInterface } from '../_objects/interfaces/lap-data.interface';
 import { LapData } from '../_objects/lapData';
 import { WebSocketService } from '../websocket.service';
+import { RoundingService } from './rounding.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ export class LapService {
 
   private lapDataArray: LapData[];
 
-  constructor(private wsService: WebSocketService) {
+  constructor(private wsService: WebSocketService,
+              private rdService: RoundingService) {
     this.lapDataArray = []
     this.lapData$ = new EventEmitter<LapData[]>();
     this.wsService.lapMultiplex$.subscribe(
@@ -32,12 +34,12 @@ export class LapService {
 
     newData.lapTime = this.getTimeString(data.secondsdifference);
 
-    newData.totalPowerIn = data.totalpowerin;
-    newData.totalPowerOut = data.totalpowerout;
-    newData.netPowerOut = data.netpowerout;
-    newData.distance = data.distance;
-    newData.amphours = data.amphours;
-    newData.averagePackCurrent = data.averagepackcurrent;
+    newData.totalPowerIn = this.rdService.getRoundedValue(data.totalpowerin, 2);
+    newData.totalPowerOut = this.rdService.getRoundedValue(data.totalpowerout, 2);
+    newData.netPowerOut = this.rdService.getRoundedValue(data.netpowerout, 2);
+    newData.distance = this.rdService.getRoundedValue(data.distance, 2);
+    newData.amphours = this.rdService.getRoundedValue(data.amphours, 2);
+    newData.averagePackCurrent = this.rdService.getRoundedValue(data.averagepackcurrent, 2);
     newData.batterySecondsRemaining = data.batterysecondsremaining;
 
     this.lapDataArray.unshift(newData)
