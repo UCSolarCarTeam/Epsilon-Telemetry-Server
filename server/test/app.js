26fa85c7206fed551afd66beef46730f626338bb
Bill Luu <billluu619@gmail.com>
2019-09-06 13:24:42 -0600

    Redo distance calculation & add unit tests for it
    The original assumption that an odometer can't decrease in value was wrong :( , so it caused massive discrepancies. We were also not rounding the values from meters to kilometers.
    
    I also added some chai unit tests, that can be run by installing mocha and running `./node_modules/mocha/bin/mocha` in the server directory.
    
    I'll do a future PR for documentation & getting travis to run it the unit tests

--- server/test/app.js
+++ server/test/app.js
@@ -1,11 +1,48 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const race = require('../scripts/race.js');
 
-const request = require('supertest');
-const app = require('../app.js');
+describe('race', function() {
+  describe('calculateMotorDistance', function() {
+    it('should get the average distance traveled between the first and last packets from the 2 odometers, returned in km', function() {
+      // Arrays should be sorted newest first
+      const packetArray = [
+        {'motor0odometer': 10000, 'motor1odometer': 1000},
+        {'motor0odometer': 5000, 'motor1odometer': 2000},
+        {'motor0odometer': 5000, 'motor1odometer': 0},
+      ];
+      assert.equal(3.0, race.getDistanceTraveled(packetArray));
+    });
 
-describe('GET /random-url', () => {
-  it('should return 404', (done) => {
-    request(app)
-      .get('/reset')
-      .expect(404, done);
+    it('should accept negative numbers as valid inputs', function() {
+      const packetArray = [
+        {'motor0odometer': -10000, 'motor1odometer': -1000},
+        {'motor0odometer': 5000, 'motor1odometer': 2000},
+        {'motor0odometer': 5000, 'motor1odometer': 0},
+      ];
+      assert.equal(-8.0, race.getDistanceTraveled(packetArray));
+    });
+
+    it('should account for when a motor resets and jumps the odometer value to zero', function() {
+      const packetArray = [
+        {'motor0odometer': 10000, 'motor1odometer': 2000},
+        {'motor0odometer': 0, 'motor1odometer': 1000},
+        {'motor0odometer': 5000, 'motor1odometer': 1000},
+        {'motor0odometer': 0, 'motor1odometer': 0},
+      ];
+      assert.equal(8.5, race.getDistanceTraveled(packetArray));
+    });
+
+    it('should not change the contents of the input array', function() {
+      const expected = [
+        {'motor0odometer': 10000, 'motor1odometer': 1000},
+        {'motor0odometer': 5000, 'motor1odometer': 2000},
+        {'motor0odometer': 5000, 'motor1odometer': 0},
+      ];
+      const actual = expected.slice(0);
+      race.getDistanceTraveled(actual);
+
+      expect(actual).to.have.ordered.deep.members(expected);
+    });
   });
 });
