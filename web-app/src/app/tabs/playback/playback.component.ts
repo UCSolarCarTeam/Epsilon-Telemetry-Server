423597b5fd4c9d3eae1cf99d0b8adf67e9f04f16
Dillon Sahadevan <dillonapple7h@gmail.com>
2022-06-16 20:15:36 -0600

    Adding some styling for AuxBms, KeyMotor, and MotorDetails

--- web-app/src/app/tabs/playback/playback.component.ts
+++ web-app/src/app/tabs/playback/playback.component.ts
@@ -64,4 +64,110 @@ export class PlaybackComponent {
   getTime(timestamp: number) {
     return new Date(timestamp).toLocaleTimeString();
   }
+
+  getPropertyStyling(property: string, value) {
+    switch (property) {
+      case 'AuxBms.AuxBmsAlive':
+        if (!value) {
+          return 'red';
+        }
+        break;
+      case 'AuxBms.AuxVoltage':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'AuxBms.ChargeContactorError':
+      case 'AuxBms.ChargeOpenButShouldBeClosed':
+      case 'AuxBms.ChargeTripDueToHighCellVoltage':
+      case 'AuxBms.ChargeTripDueToHighTemperatureAndCurrent':
+      case 'AuxBms.ChargeTripDueToPackCurrent':
+      case 'AuxBms.CommonContactorError':
+      case 'AuxBms.DischargeOpenButShouldBeClosed':
+      case 'AuxBms.DischargeTripDueToHighTemperatureAndCurrent':
+      case 'AuxBms.DischargeTripDueToLowCellVoltage':
+      case 'AuxBms.DischargeTripDueToPackCurrent':
+      case 'AuxBms.ProtectionTrip':
+        if (value) {
+          return 'red';
+        }
+        break;
+      case 'KeyMotor.BusCurrent':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'KeyMotor.BusVoltage':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.PhaseCCurrent':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.PhaseBCurrent':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.MotorVoltageImaginary':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.MotorCurrentReal':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.MotorCurrentImaginary':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.BackEmf':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.VoltageRail15VSupply':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.VoltageRail3VSupply':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.VoltageRail1VSupply':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.HeatSinkTemp':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.MotorTemp':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.DspBoardTemp':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+      case 'MotorDetails.DcBusAmpHours':
+        if (value > 12.5 || value < 11.5) {
+          return 'red';
+        }
+        break;
+    }
+    return 'white';
+  }
 }
